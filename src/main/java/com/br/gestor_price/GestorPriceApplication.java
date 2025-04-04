ed857ce498442284e0147d723540c1c05091cd75
Ramon Duarte <ramon_duarte.10@hotmail.com>
2025-04-04 06:54:17 -0300

    iniciando o projeto

---
+++ src/main/java/com/br/gestor_price/GestorPriceApplication.java
@@ -0,0 +1,13 @@
+package com.br.gestor_price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gestor_price/GestorPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gestor_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
